1370ab124384f0b5ff30625d5d32148fc4c70ecb
GangHan-CU <72283104+GangHan-CU@users.noreply.github.com>
2021-10-25 15:00:39 -0400

    Update PayRent.java
    Added some comments for PayRent

--- src/PayRent.java
+++ src/PayRent.java
@@ -1,5 +1,8 @@
 import java.util.List;
-
+/*
+*   @author Gang Han
+*   Process the rent payment
+*/
 public class PayRent {
 
     private Players tenant;
@@ -14,22 +17,30 @@ public class PayRent {
         this.players = players;
         this.property = board.propertyholder.get(tenant.getPosition());
 
+        //If the property is owned
         if(property.isOwned() == true){
+            //Loop through player list to find the owner
             for(int i = 0; i < 4; i++){
+                //Once the owner is found
                 if(property.getOwner() == players.get(i).getName()){
+                    //Assign landLord
                     landLord = players.get(i);
                     paymentProgress();
                 }
             }
+            //When the property is not owned by anyone
         }else{
             System.out.println("This property is not owned by anyone... yet");
         }
     }
 
-
+    //Go through the payment
     public void paymentProgress(){
+        //Create a local integer of rent
         int rent = this.property.getRent(this.property.getCost());
+        //Do payment for tenant
         tenant.setMoney(tenant.getMoney() - rent);
+        //Landlord receive the payment
         landLord.setMoney(landLord.getMoney() + rent);
     }
 
