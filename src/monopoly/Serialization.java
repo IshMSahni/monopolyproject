070f96d470e707ac80e69f1d9b394fdc347804a3
Ishanov Sahni <33579076+IshMSahni@users.noreply.github.com>
2021-12-06 19:01:14 -0500

    added serialization files used for writing and reading from files

--- src/monopoly/Serialization.java
+++ src/monopoly/Serialization.java
@@ -9,6 +9,45 @@ import java.util.Date;
 
 public class Serialization {
 
+    public static ByteArrayOutputStream write(Serializable s){
+        try {
+            ByteArrayOutputStream byte_array_output_stream = new ByteArrayOutputStream();
+            ObjectOutputStream object_output_stream = new ObjectOutputStream(byte_array_output_stream);
+            object_output_stream.writeObject(s);
+            object_output_stream.close();
+            return byte_array_output_stream;
+        } catch (Throwable t) {
+            t.printStackTrace(System.err);
+            return null;
+        }
+    }
 
+    public static String write_base64(Serializable s){
+        ByteArrayOutputStream os = write(s);
+        if(os != null){
+            return Base64.getEncoder().encodeToString(os.toByteArray());
+        }
+        else{
+            return "";
+        }
+    }
+
+    public static Object read(ByteArrayInputStream byte_array_input_stream){
+        Object object = null;
+        try {
+            ObjectInput input = new ObjectInputStream(byte_array_input_stream);
+            object = input.readObject();
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+    public static Object read_base64(String base64_str){
+        ByteArrayInputStream byte_array_input_stream = new ByteArrayInputStream(Base64.getDecoder().decode(base64_str));
+
+        return read(byte_array_input_stream);
+    }
 
 }
--- src/test/monopoly/SerializationTest.java
+++ src/test/monopoly/SerializationTest.java
@@ -8,6 +8,34 @@ import java.io.Serializable;
 
 public class SerializationTest {
 
+    static class TestObject implements Serializable {
+        public int i;
+        public String s;
+        public TestObject(int i, String s) {
+            this.i = i;
+            this.s = s;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return s.equals(((TestObject) o).s) && i == ((TestObject) o).i;
+        }
+
+    }
+
+    @Test
+    public void write_base64() {
+        TestObject testObject = new TestObject(1, "test");
+        String serialized = Serialization.write_base64(testObject);
+        Assert.assertEquals("rO0ABXNyACp0ZXN0Lm1vbm9wb2x5LlNlcmlhbGl6YXRpb25UZXN0JFRlc3RPYmplY3QSnZDMzQwS7wIAAkkAAWlMAAFzdAASTGphdmEvbGFuZy9TdHJpbmc7eHAAAAABdAAEdGVzdA==", serialized);
+    }
+
+    @Test
+    public void read_base64() {
+        TestObject testObject = new TestObject(1, "test");
+        TestObject from_base64 = (TestObject) Serialization.read_base64("rO0ABXNyACp0ZXN0Lm1vbm9wb2x5LlNlcmlhbGl6YXRpb25UZXN0JFRlc3RPYmplY3QSnZDMzQwS7wIAAkkAAWlMAAFzdAASTGphdmEvbGFuZy9TdHJpbmc7eHAAAAABdAAEdGVzdA==");
+        Assert.assertTrue(testObject.equals(from_base64));
+    }
 
 
 
